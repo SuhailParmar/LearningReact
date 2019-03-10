709569f19fc67d56a12173819720b5d5f817f848
Suhail Parmar <suhailparmar@zenyatta.lan>
2019-03-10 10:59:40 +0000

    Booyah!

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -25,6 +25,11 @@ export default class ResultsPage extends Component {
         {
           time_day_worded: "Tue",
           time_year: "2019",
+          junction: 2
+        },
+        {
+          time_day_worded: "Tue",
+          time_year: "2019",
           junction: 3
         },
         {
@@ -37,74 +42,76 @@ export default class ResultsPage extends Component {
   }
 
   getEventsFromAPI = () => {
-    console.log(this.props.url);
-    authenticatedRequest(this.props.url).then(result => {
-      var parsedEvents = [];
-      result.forEach(string => {
-        var parsedEvent = JSON.parse(string);
-        parsedEvent.closest_cities = parsedEvent.closest_cities[0];
-        parsedEvent.junction = parsedEvent.junction[0];
-        parsedEvents.push(parsedEvent);
+    if (this.props.url) {
+      authenticatedRequest(this.props.url).then(result => {
+        var parsedEvents = [];
+        result.forEach(string => {
+          var parsedEvent = JSON.parse(string);
+          parsedEvent.closest_cities = parsedEvent.closest_cities[0];
+          parsedEvent.junction = parsedEvent.junction[0];
+          parsedEvents.push(parsedEvent);
+        });
+        if (parsedEvents !== []) this.setState({ data: parsedEvents });
       });
-      if (parsedEvents !== []) this.setState({ data: parsedEvents });
-    });
+    }
   };
 
   componentDidMount() {
     this.getEventsFromAPI();
   }
 
-  chartRender = () => {
-    let junctions = this.preProcess();
-
-    return (
-      <div>
-        <h3>Number of accidents per Junction</h3>
-        <VictoryGroup>
-          <VictoryBar
-            barWidth={5}
-            data={junctions}
-            domain={{ x: [1, 20] }}
-            labels={l => `J${l.x}:${l.y}`}
-            sortKey="x"
-          />
-        </VictoryGroup>
-      </div>
-    );
-  };
-
-  preProcess = () => {
+  renderIncidentsPerJunction = () => {
     // Create a graph based on number of
     // Events PER Junction
     let dataCopy = this.state.data;
     // Array of JSON, x=Junction Number {x : 25, y: 1}
-    let data = [];
-
+    let junctions = [];
     dataCopy.forEach(event => {
       let j = event.junction;
       let found = false;
       // Check if the junction already exists
-      for (var i = 0; i < data.length; i++) {
-        if (data[i].x === j) {
+      for (var i = 0; i < junctions.length; i++) {
+        if (junctions[i].x === j) {
           found = true;
-          data[i].y += 1;
+          junctions[i].y += 1;
           break;
         }
       }
       if (!found) {
-        data.push({ x: j, y: 1 });
+        junctions.push({ x: j, y: 1 });
       }
     });
 
-    console.log(data);
-    return data;
+    // Create the values for the Y axis
+    let ticky = [];
+    junctions.forEach(j => {
+      ticky.push(j.y);
+    });
+
+    return (
+      <div className="incPerJunc">
+        <h3>Number of incidents per Junction</h3>
+        <VictoryChart domainPadding={10}>
+          <VictoryAxis />
+          <VictoryAxis dependentAxis tickFormat={ticky} />
+          <VictoryBar
+            barWidth={5}
+            data={junctions}
+            domain={{ x: [1, 20] }}
+            labels={l => `J${l.x}`}
+            sortKey="x"
+          />
+        </VictoryChart>
+      </div>
+    );
   };
 
   render() {
     return (
       <div className="resultsPage">
         <h1>Here are your results!</h1>
-        <this.chartRender />
+        Query: {this.props.url}
+        <this.renderIncidentsPerJunction />
       </div>
     );
   }
